5520cdfbf553d8e2dff710b86084f0979d396c62
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2022-04-25 10:06:21 +0000

    use file as DB, create TrainingResult
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- src/training.py
+++ src/training.py
@@ -1,4 +1,4 @@
-import json
+import torch
 from typing import Any, ByteString, Dict
 import pandas as pd
 import pickle
@@ -14,15 +14,19 @@ from proloaf.modelhandler import ModelWrapper
 def parse_run_training(json_message_body):
     job = TrainingJob(**json_message_body)  # , default=str))
     job.status = JobStatus.doing
-    redis_job.set(f"model_{job.job_id}", job.json())
+    # redis_job.set(f"model_{job.job_id}", job.json())
     return job
 
 
 def handle_run_training(basetraining: TrainingBase):
-    training_def = basetraining.training_configuration
+    training_def = basetraining.training
     if isinstance(basetraining.model, int):
-        pyd_model = pickle.loads(redis_model.get(f"model_{basetraining.model}"))
-        model = pyd_model.model_object
+        # pyd_model = pickle.loads(redis_model.get(f"model_{basetraining.model}"))
+        pyd_model = torch.load("model.pkl")
+        # pyd_model.reinitialize()
+        print("")
+        print(pyd_model)
+        model = pyd_model.model
     else:
         model = ModelWrapper(
             **basetraining.model.model_definition.dict(),
@@ -35,27 +39,34 @@ def handle_run_training(basetraining: TrainingBase):
         )
         model.init_model()
         pyd_model = PredModel(
-            **basetraining.model.dict(), model_id=get_unique_id(), model_object=model
+            **basetraining.model.dict(),
+            model=model,
+            model_id=1,  # TODO model_id=get_unique_id()
         )
-    df = pd.read_csv("opsd.csv", sep=";")
+    pyd_model.model.training = training_def
+    df = pd.read_csv("proloaf-worker/opsd.csv", sep=";")
     df_train, df_val = dh.split(df, [0.9])
     # TODO add selection of source
     train_data = tl.TimeSeriesData(df_train, **training_def.dict())
     val_data = tl.TimeSeriesData(df_val, **training_def.dict())
+    print(model.dict())
     model.run_training(
         train_data=train_data,
         validation_data=val_data,
         batch_size=training_def.batch_size,
     )
 
-    pyd_model.date_trained = model.last_training.training_start_time
-    pyd_model.predicted_feature = training_def.target_id
+    pyd_model.date_trained = model.training.training_start_time
+    pyd_model.predicted_feature = model.target_id[0]
     # TODO add expected dataformat to pyd_model
-    redis_model.set(f"model_{pyd_model.model_id}", pickle.dumps(pyd_model))
+    # TODO redis_model.set(f"model_{pyd_model.model_id}", pickle.dumps(pyd_model))
 
-    dt = model.last_training.training_end_time - model.last_training.training_start_time
+    dt = model.training.training_end_time - model.training.training_start_time
     return TrainingResult(
+        training=model.training.get_config(),
+        model=pyd_model,
+        data=basetraining.data, # TODO data should be selfdocumenting
         actual_training_time=dt,
-        validation_error=model.last_training.validation_loss,
-        training_error=model.last_training.training_loss,
+        validation_error=model.training.validation_loss,
+        training_error=model.training.training_loss,
     )
