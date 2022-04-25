043e5589a00c4998bd339e0cbaa42e340df5b584
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2022-04-25 10:07:44 +0000

    fix initialization, unify naming
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- src/schemas/training.py
+++ src/schemas/training.py
@@ -1,27 +1,32 @@
 from __future__ import annotations
 from typing import Optional, Dict, Any, Union
+from proloaf.modelhandler import TrainingRun
+from proloaf.base import PydConfigurable
 
-from pydantic import Field, BaseModel
+
+from pydantic import Field
 from .data import InputData
 from .job import Job
 from .model import PredModelBase
+
+BaseModel = PydConfigurable
 
 # Training
 class TrainingBase(BaseModel):
     data: InputData
     max_training_time: Optional[int] = Field(
-        ..., ge=0, description="Maximum training duration in seconds"
+        None, ge=0, description="Maximum training duration in seconds"
     )
     model: Union[int, PredModelBase] = Field(
         ...,
         description="ID of an existing model or definition for a new one that is to be trained",
     )
-    training_configuration: Optional[ProloafTrainingDefinition]
+    training: TrainingRun
 
 
 class TrainingResult(TrainingBase):
     actual_training_time: Optional[int] = Field(
-        ..., ge=0, description="Actual time spend on training duration in seconds"
+        None, ge=0, description="Actual time spend on training duration in seconds"
     )
     validation_error: Optional[float]
     training_error: float
@@ -32,7 +37,6 @@ class TrainingJob(Job):
     result: Optional[TrainingResult]
 
 
-# TODO
 class ProloafTrainingDefinition(BaseModel):
     target_id: str = "load"
     optimizer_name: str = "adam"
