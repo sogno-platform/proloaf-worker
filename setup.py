57f17cb455d7d4c4be272b4ce144d681eb28c392
Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>
2022-11-02 14:50:40 +0000

    update dependency to develop
    Signed-off-by: Florian Oppermann <florian.oppermann@eonerc.rwth-aachen.de>

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ setup(
     version="0.1",
     description="Classes for interfacing with AMQP",
     author="Florian Oppermann",
-    url="https://github.com/sogno-platform/sogno-job-queue",
+    url="https://github.com/sogno-platform/proloaf-worker",
     packages=find_packages()  ,#["sognoforecasting"],
     license="Apache-2.0 License",
     python_requires=">=3.8",
     install_requires=[
         "pydantic ~= 1.9",
         "redis ~= 4.1",
-        "sogno-job-queue @ git+https://github.com/sogno-platform/sogno-job-queue@main#egg=sogno-job-queue",
+        "sogno-job-queue @ git+https://github.com/sogno-platform/sogno-job-queue@develop#egg=sogno-job-queue",
         "proloaf @ git+https://git.rwth-aachen.de/acs/public/automation/plf/proloaf/@prep-for-sogno-service-new-history#egg=proloaf",
     ],
 )
